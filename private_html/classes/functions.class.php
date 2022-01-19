824c90190db642a70bc7d8f6f1888dccbc6ced34
Oscar Freij <otg020313@gmail.com>
2022-01-20 00:55:55 +0100

    Add account functions
    Added account functions:
    login, checks user access then sets session variables.
    logout, destroys session.
    checkLogin, checks login creds with database.
    getUserData, gathers userdata specified user.

--- private_html/classes/functions.class.php
+++ private_html/classes/functions.class.php
@@ -8,5 +8,51 @@ class functions
     {
         $this->container = $container;    
     }
+
+    private function checkLogin($username, $password)
+    {
+        $quotedUsername = $this->container->db()->quote($username);
+        $result = $this->container->db()->constructResultQuerry("SELECT `password`, `enabled` FROM `users` WHERE `users.username` = $quotedUsername AND `users.enabled` = 1;");
+
+        if ($result !== false && count($result) == 0)
+        {
+            $hash = $result[0]['password'];
+            if ($result[0]['enabled'] == 1)
+            {
+                return password_verify($password, $hash);
+            }
+        }
+        return false;
+    }
+
+    private function getUserData($username)
+    {
+        $quotedUsername = $this->container->db()->quote($username);
+        return $result = $this->container->db()->constructResultQuerry("SELECT `id`, `teamAccess`, `superAdmin` FROM `users` WHERE `users.username` = $quotedUsername AND `users.enabled` = 1;");
+    }
+
+    public function login($username, $password)
+    {
+        if ($this->checkLogin($username, $password))
+        {
+            $result = $this->getUserData($username);
+
+            session_start();
+            $_SESSION['id'] = $result[0]['id'];
+            $_SESSION['username'] = $username;
+            $_SESSION['teamAccess'] = $result[0]['teamAccess'];
+            $_SESSION['superAdmin'] = $result[0]['superAdmin'];
+        }
+    }
+
+    public function logout()
+    {
+        session_start();
+        session_destroy();
+
+        $host  = $_SERVER['HTTP_HOST'];
+        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        header("Location: http://$host$uri/");
+    }
 }
 ?>
