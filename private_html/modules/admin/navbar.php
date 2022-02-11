3efeae70c8ea26b94ca4924671cb6b15f6dd6266
Oscar Freij <otg020313@gmail.com>
2022-02-11 08:39:03 +0100

    Add admin pages and navbar base
    Added base files for admin view and navbar structure.
    Also added image to use as logo

---
+++ private_html/modules/admin/navbar.php
@@ -0,0 +1,11 @@
+<div id="navbar">
+    <div class="navbar-logo">
+        <img src="static/media/admin-text.png" alt="">
+        <span>RollCall</span>
+    </div>
+    <div class="navbar-links">
+        <a class="navbar-link" href="?page=admin&subpage=employees">Employees</a>
+        <a class="navbar-link" href="?page=admin&subpage=teams">Teams</a>
+        <a class="navbar-link" href="?page=admin&subpage=admins">Admins</a>
+    </div>
+</div>
